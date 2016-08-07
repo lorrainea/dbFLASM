8c9ce738ae271848d08fadae009205da8b752322
lorrainea <lorraineayad@hotmail.co.uk>
2016-08-07 21:50:07 +0100

    update

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 /**
-    libFLASM
-    Copyright (C) 2016 Lorraine A. K. Ayad, Solon P. Pissis and Ahmad Retha
+    dbFLASM
+    Copyright (C) 2016 Lorraine A. K. Ayad and Solon P. Pissis
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
